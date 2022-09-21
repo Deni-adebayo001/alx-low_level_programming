a6d009fc81d4324fc184cb7f2e740822c04e0d09
Deni-adebayo001 <deniabimbola@gmail.com>
2022-09-21 19:33:16 +0000

    Done

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *rot13 - encodes a string in rot13
+ *@s: string to be encoded
+ *Return: the resulting string
+ */
+char *rot13(char *s)
+{
+	int i, j;
+
+	char a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char b[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; a[j] != '\0'; j++)
+		{
+			if (s[i] == a[j])
+			{
+				s[i] = b[j];
+				break;
+			}
+		}
+	}
+
+	return (s);
+}
+int main(void)
+{
+	char ch[] = "Emmanuel Ugbem";
+	printf("%s\n", rot13(ch));
+	return (0);
+}
+		
